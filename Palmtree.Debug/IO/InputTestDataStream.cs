a0510adee542ff42ad3f2a1817f474addc7d609d
rougemeilland <28302784+rougemeilland@users.noreply.github.com>
2024-01-14 21:58:00 +0900

    デバッグ用コードのプロジェクトを追加

---
+++ Palmtree.Debug/IO/InputTestDataStream.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Palmtree.Collections;
+using Palmtree.IO;
+using Palmtree.IO.StreamFilters;
+
+namespace Palmtree.Debug.IO
+{
+    public class InputTestDataStream
+        : SequentialInputByteStreamFilter
+    {
+        private const Int32 _MAX_BUFFER_SIZE = 1024 * 1024;
+
+        private InputTestDataStream(ISequentialInputByteStream baseStream)
+            : base(baseStream, false)
+        {
+        }
+
+        public static InputTestDataStream Create(UInt64 length, Func<Byte, Byte>? byteDataFilter = null)
+        {
+            if (length < (sizeof(UInt64) + sizeof(UInt32)))
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            var pipe = new InProcessPipe();
+            _ = Task.Run(() =>
+            {
+                using var outStream = pipe.OpenOutputStream();
+                var contentLength = checked(length - (sizeof(UInt64) + sizeof(UInt32)));
+                outStream.WriteUInt64LE(contentLength);
+
+                var crcValueHolder = new ValueHolder<(UInt32 crc, UInt64 length)>();
+                using (var contentStream = outStream.WithCrc32Calculation(crcValueHolder, true))
+                {
+                    var remain = contentLength;
+                    while (remain > 0)
+                    {
+                        var lengthToWrite = checked((Int32)remain.Minimum((UInt64)_MAX_BUFFER_SIZE));
+                        var testDataSequence = RandomSequence.GetByteSequence().Take(lengthToWrite);
+                        if (byteDataFilter is not null)
+                            testDataSequence = testDataSequence.Select(b => byteDataFilter(b));
+                        foreach (var btteData in testDataSequence)
+                        {
+                            contentStream.WriteByte(btteData);
+                        }
+
+                        checked
+                        {
+                            remain -= (UInt64)lengthToWrite;
+                        }
+                    }
+                }
+
+                outStream.WriteUInt32LE(crcValueHolder.Value.crc);
+            });
+            return new InputTestDataStream(pipe.OpenInputStream());
+        }
+    }
+}
---
+++ Palmtree.Debug/IO/OutputTestDataStream.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Palmtree.IO;
+using Palmtree.IO.StreamFilters;
+
+namespace Palmtree.Debug.IO
+{
+    public class OutputTestDataStream
+        : SequentialOutputByteStreamFilter
+    {
+        private Exception? _exception;
+
+        private OutputTestDataStream(ISequentialOutputByteStream baseStream)
+            : base(baseStream, false)
+        {
+        }
+
+        public static OutputTestDataStream Create(Action<Exception> exceptionHandler)
+        {
+            var endOfReading = new ManualResetEventSlim();
+            var pipe = new InProcessPipe();
+            var instance =
+                new OutputTestDataStream(
+                    pipe.OpenOutputStream()
+                    .WithEndAction(
+                        _ =>
+                        {
+                            endOfReading.Wait();
+                            endOfReading.Dispose();
+                        }));
+            _ = Task.Run(() =>
+            {
+                try
+                {
+                    using var inStream = pipe.OpenInputStream();
+                    var contentLength = inStream.ReadUInt64LE();
+                    var (actualCrc, length) = inStream.WithPartial(contentLength, true).CalculateCrc32();
+                    var crc = inStream.ReadUInt32LE();
+                    if (crc != actualCrc)
+                        throw new Exception("The output test data is incorrect.");
+                    if (inStream.ReadBytes(1).Length > 0)
+                        throw new Exception("The output test data is incorrect.");
+                }
+                catch (Exception ex)
+                {
+                    lock (instance)
+                    {
+                        instance._exception = ex;
+                    }
+
+                    try
+                    {
+                        exceptionHandler(ex);
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                finally
+                {
+                    endOfReading.Set();
+                }
+            });
+            return instance;
+        }
+
+        protected override Int32 WriteCore(ReadOnlySpan<Byte> buffer)
+        {
+            ThrowIfError();
+            return base.WriteCore(buffer);
+        }
+
+        protected override Task<Int32> WriteAsyncCore(ReadOnlyMemory<Byte> buffer, CancellationToken cancellationToken)
+        {
+            ThrowIfError();
+            return base.WriteAsyncCore(buffer, cancellationToken);
+        }
+
+        protected override void FlushCore()
+        {
+            ThrowIfError();
+            base.FlushCore();
+        }
+
+        protected override Task FlushAsyncCore(CancellationToken cancellationToken = default)
+        {
+            ThrowIfError();
+            return base.FlushAsyncCore(cancellationToken);
+        }
+
+        private void ThrowIfError()
+        {
+            lock (this)
+            {
+                if (_exception is not null)
+                {
+                    var ex = _exception;
+                    _exception = null;
+                    throw ex;
+                }
+            }
+        }
+    }
+}
